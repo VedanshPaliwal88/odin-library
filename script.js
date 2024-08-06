babd5063df4a741bc651b60f20bb78fd91ee586c
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2024-08-07 00:12:14 +0530

    added js form validation for new book

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const isRead = document.getElementById("isRead");
 
+const titleError = document.querySelector("#title + span.error");
+const authorError = document.querySelector("#author + span.error");
+const pagesError = document.querySelector("#pages + span.error");
 
 
 class Book {
@@ -95,14 +98,33 @@ addBook.addEventListener("click", () => {
 
 dialog.addEventListener('close', (e) => {
     const bookInfo = dialog.returnValue.split(',')
+    if(bookInfo === null) console.log(...bookInfo);
     let book = new Book(...bookInfo);
     myLibrary.addBookToLibrary(book);
 })
 
 confirmBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    let returns = [title.value, author.value, pages.value, isRead.value];
-    dialog.close(returns);
-})
+    if (title.validity.valid && author.validity.valid && pages.validity.valid) {
+        let returns = [title.value, author.value, pages.value, isRead.value];
+        dialog.close(returns);
+    } else showError();
+});
 
 
+function showError() {
+    if (title.validity.valueMissing) {
+        titleError.textContent = "Please enter a title";
+        titleError.className = "error active";
+    }
+
+    if (author.validity.valueMissing) {
+        authorError.textContent = "Please enter author's name";
+        authorError.className = "error active";
+    }
+    
+    if (pages.validity.valueMissing) {
+        pagesError.textContent = "Please enter number of pages";
+        pagesError.className = "error active";    
+    }
+}
